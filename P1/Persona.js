021f679f0b33ac17780380c31ae02fe7932a07fc
Samuel <samuguiel@gmail.com>
2021-03-11 12:33:34 +0100

    Creación del personaje Sara

--- P1/Persona.js
+++ P1/Persona.js
@@ -33,36 +33,36 @@ angular.module("angular", [])
                 
                 
 				Id: 0,
-				Name: "Pedro",
+				Name: "Sara",
 				Photo: "man.png",
 				Quote: "Carpe Diem",
-				Age: 32,
-				Occupation: "Comercial en empresa de automocion",
+				Age: 25,
+				Occupation: "Estudiante de un máster de energía nuclear",
 				Family: "Con pareja desde hace 1 año",
-				Location: "Granada (Huetor Vega)",
-				Character: "Le gusta disfrutar del tiempo",
+				Location: "Madrid Capital",
+				Character: "Le gusta descubrir lugares nuevos y salir con sus amigos de cervezas",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 2 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 4 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
 				], 
-				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
-				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
-				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
-				Tech: [
+				Goals: ["Terminar el master y encontrar un trabajo que le motive en un lugar en el que pueda vivir con su novio, descubrir mundo"],
+				Frustrations: ["le gusta mucho planear viajes con sus amigos y su novio pero el master le ocupa mucho tiempo", "Le gustaría tener una mejor relación con su familia con la que apenas se habla"],
+				Bio: "Es de toledo y se fue a madrid a estudiar un grado de ingniería de la energía y actualmente esta estudiando un máster. Ha encontrado trabajos esporádicos como camarera. A su grupo de amigos y a su novio actual los conoció en la carrera y es con lo que queda siempre para salir y hacer viajes.",
+				Tech: [ 
 					{ Name: "TIC/Internet", Value: 2 },
-					{ Name: "Movil", Value: 2 },
-					{ Name: "RRSS", Value: 3 },
+					{ Name: "Movil", Value: 3 },
+					{ Name: "RRSS", Value: 4 },
 					{ Name: "Software", Value: 2 }
 					
 				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+                Contextos: "Está a punto de terminar el máster y lo quiere celebrar por todo lo alto haciendo un viajazo con sus amigos y su novio",  
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional", Value: 5 },
-					{ Name: "Online & Social Media", Value: 3 },
+					{ Name: "Publicidad Tradicional", Value: 2 },
+					{ Name: "Online & Social Media", Value: 4 },
 					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 3 }
 				]
 			},
 			{	
