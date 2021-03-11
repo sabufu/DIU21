adce09573d1191e4b8d79666c4e35d5c23dca47a
AgLuque1 <79659314+AgLuque1@users.noreply.github.com>
2021-03-11 16:31:26 +0100

    Add files via upload

--- P1/Persona.js
+++ P1/Persona.js
@@ -74,35 +74,35 @@ angular.module("angular", [])
                 
                 
 				Id: 1,
-				Name: "Monica Suarez",
+				Name: "Nuria",
 				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Quote: "Los recuerdos nunca mueren",
+				Age: 48,
+				Occupation: "Escritora",
+				Family: "Viuda con dos hijas",
+				Location: "Oviedo (Asturias)",
+				Character: "Le encanta pasar tiempo con sus hijas.",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 2 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 5 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 4 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Goals: ["Escribir con el mar de fondo", "Escribir una historia con sus hijas", "Escribir una decalogía"],
+				Frustrations: ["No creerse suficiente para sus hijas", "No tener tiempo suficiente para escribir todo lo que quiere"],
+				Bio: "Nació y se crió en Oviedo junto a sus padres, la pasión por la lectura y los libros nació cuando empezó el colegio. Su género favorito siempre ha sido la fantasía y la ciencia ficción. Hace un par de años perdió a su marido en un accidente de coche, y desde entonces prefiere pasar más tiempo a solas excepto cuando está con sus hijas, por eso ha decidido hacer un viaje con ellas.",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
-					{ Name: "Mobile", Value: 3 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
+					{ Name: "TIC/Internet", Value: 3 },
+					{ Name: "Mobile", Value: 4 },
+					{ Name: "RRSS", Value: 2 },
+					{ Name: "Software", Value: 1 }
 					
 				], 
-                Contextos:   "The goals this user hopes to achieve." ,
+                Contextos:   "Lleva un tiempo pensando en hacer un viaje con sus hijas, y ha decidido hacerlo" ,
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
+					{ Name: "Publicidad Tradicional (Ads)", Value: 4 },
 					{ Name: "Online & Social Media", Value: 2 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
 					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
 				]
 			}
